b5b824501b446657c09cf9fecfc5a8e7dc5b69cd
santilobos <santilobos21@gmail.com>
2024-02-15 09:13:44 +0100

    he hecho solo estos ejercicios

---
+++ index.js
@@ -0,0 +1,22 @@
+let arrayVacio = []
+
+let arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] 
+
+let arrayNumerosPares = [0, 2, 4, 6, 8]
+
+let arrayBidimensional = [[0, 1, 2], ['a', 'b', 'c']]
+
+function suma(num1, num2) {
+    return num1 + num2;
+  }
+
+
+  // Aqui en el ejercicio no pone ninguna restricción, pero en Jasmine pone prohibido usar operador **; Aún así lo toma como válido
+
+  function potenciacion(num1, num2){
+    return num1**num2;
+  }
+
+  function separarPalabras(string) {
+    return ['hola mundo' > [hola, mundo]];
+  }
